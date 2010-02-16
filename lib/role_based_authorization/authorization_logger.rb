10e48b45aaa9a15ebfb961d371a2684553d021d2
Roberto Esposito <boborbt@gmail.com>
2010-02-16 15:04:59 +0100

    Commented AuthorizationLogger

--- lib/role_based_authorization/authorization_logger.rb
+++ lib/role_based_authorization/authorization_logger.rb
@@ -1,6 +1,10 @@
+# Personalizes the authorization logging by adding Auth | in front of each line.
+# It also outputs the time stamp and the severity of the log message.
 class AuthorizationLogger < Logger
+  # Prefix for each message
   AUTHORIZATION_SYSTEM_LOG_MSG_PREFIX = 'Auth |'
   
+  # overriding of the formatting message
   def format_message(severity, timestamp, progname, msg)
       "#{AUTHORIZATION_SYSTEM_LOG_MSG_PREFIX} #{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n" 
   end

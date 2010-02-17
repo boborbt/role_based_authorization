b5ae553167908bc1748acdd197c89f7ba9910421
boborbt <boborbt@gmail.com>
2010-02-17 23:58:27 +0100

    yet another authorize_action? cleanup

--- lib/role_based_authorization/role_based_authorization.rb
+++ lib/role_based_authorization/role_based_authorization.rb
@@ -21,7 +21,7 @@ module RoleBasedAuthorization
   def exists_matching_rule_for_this_controller? rules_for_controller, options
     user,actions,ids = *options.values_at(:user, :actions, :ids)
     
-    actions.find do |action|
+    return actions.find do |action|
       AUTHORIZATION_LOGGER.debug('current action: %s' % [action])      
       action = action.to_sym
       rules_for_action = rules_for_controller[action]
@@ -34,7 +34,7 @@ module RoleBasedAuthorization
   def exists_matching_rule? options
     rules = self.class.role_auth_rules
     
-    found_matching_rule = options[:controllers].find do |controller|    
+    return options[:controllers].find do |controller|    
       AUTHORIZATION_LOGGER.debug("current controller: %s" % [controller])
 
       rules_for_controller = rules[controller]
@@ -48,34 +48,34 @@ module RoleBasedAuthorization
 
       rules_for_controller && exists_matching_rule_for_this_controller?(rules_for_controller, options)
     end
-    
-    return found_matching_rule
   end
     
   # Main authorization logic. opts is an hash with the following keys
   # :user, :controller, :action:: self explanatory
   # :ids:: id to be used to retrieve relevant objects
-  def authorize_action? opts = {} 
+  def do_authorize_action? opts
     # exiting immediately if not logged in
-    if respond_to?(:logged_in?) && !logged_in?
-      AUTHORIZATION_LOGGER.info("returning false (not logged in)")
-      return false
-    end
+    return false if respond_to?(:logged_in?) && !logged_in?
     
     opts.reverse_merge!( :user => current_user, :controller => controller_name, :ids => {} )
-    opts[:ids].reverse_merge!( opts.reject { |key,value| key.to_s !~ /(_id\Z)|(\Aid\Z)/ } )
+    user, controller, action, ids = opts.values_at( :user, :controller, :action, :ids )
+    ids.reverse_merge!( opts.reject { |key,value| key.to_s !~ /(_id\Z)|(\Aid\Z)/ } )
     
+    new_options = { :user         => user, 
+                    :controllers  => [controller,'application'], 
+                    :actions      => [:all,action], 
+                    :ids          => ids }
+                    
+    return exists_matching_rule?( new_options ) != nil
+  end
+  
+  # wraps some logging around do_authorize_action?.
+  def authorize_action? opts = {}
     AUTHORIZATION_LOGGER.info("access request. options: %s" % [opts.inspect])
-
-    new_options = { :user         => opts[:user], 
-                    :controllers  => [opts[:controller],'application'], 
-                    :actions      => [:all,opts[:action]], 
-                    :ids          => opts[:ids] }
-    result = exists_matching_rule?( new_options ) != nil
-                                    
+    result = do_authorize_action? opts
     AUTHORIZATION_LOGGER.info("returning #{result}")
-
-    result
+    
+    return result
   end
   
   
